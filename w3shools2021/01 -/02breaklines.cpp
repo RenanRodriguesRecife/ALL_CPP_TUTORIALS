b9001244df33af251bba93b8e9944aea4b20b95e
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-10-27 18:52:46 -0300

    variáveis

--- w3shools2021/01 -/02breaklines.cpp
+++ w3shools2021/01 -/02breaklines.cpp
@@ -2,6 +2,10 @@
 
 
 int main(){
+    int myNum =15;
+    myNum = 10;
+    std::cout << myNum << "\n";
+
     std::cout << "Hello World!\n";
     std::cout << "I am learning C++" << std::endl;
     std::cout << "dev c++";
